870e41376a6a747a87caee8c85b98a528c9e3bcb
leuzaoo <leuzaoocontato@gmail.com>
2022-05-31 16:17:16 -0300

    remove-ts

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inputElement = document.querySelector(".new-task-input");
-var addTaskButton = document.querySelector(".new-task-button");
-var validateInput = function () { return inputElement.value.trim().length > 0; };
-var handleAddTask = function () {
-    var inputIsValid = validateInput();
-    console.log(inputIsValid);
-    if (!inputIsValid) {
-        return inputElement.classList.add("error");
-    }
+const inputElement = document.querySelector(".new-task-input");
+const addTaskButton = document.querySelector(".new-task-button");
+
+const validateInput = () => inputElement.value.trim().length > 0;
+
+const handleAddTask = () => {
+  const inputIsValid = validateInput();
+
+  console.log(inputIsValid);
+
+  if (!inputIsValid) {
+    return inputElement.classList.add("error");
+  }
 };
-var handleInputChange = function () {
-    var inputIsValid = validateInput();
-    if (inputIsValid) {
-        return inputElement.classList.remove("error");
-    }
+
+const handleInputChange = () => {
+  const inputIsValid = validateInput();
+
+  if (inputIsValid) {
+    return inputElement.classList.remove("error");
+  }
 };
-addTaskButton.addEventListener("click", function () { return handleAddTask(); });
+
+addTaskButton.addEventListener("click", () => handleAddTask());
