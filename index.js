8fba3c4620bdc14808344133c76a46331bf07596
leuzaoo <leuzaoocontato@gmail.com>
2022-05-31 21:01:34 -0300

    js-update

--- index.js
+++ index.js
@@ -1,71 +1,53 @@
-const inputElement = document.querySelector(".new-task-input");
-const addTaskButton = document.querySelector(".new-task-button");
-
-const tasksContainer = document.querySelector(".tasks-container");
-
-const validateInput = () => inputElement.value.trim().length > 0;
-
-const handleAddTask = () => {
-  const inputIsValid = validateInput();
-
-  console.log(inputElement);
-
-  if (!inputIsValid) {
-    return inputElement.classList.add("error");
-  }
-
-  const taskItemContainer = document.createElement("div");
-  taskItemContainer.classList.add("task-item");
-
-  const taskContent = document.createElement("p");
-  taskContent.innerText = inputElement.value;
-
-  taskContent.addEventListener("click", () => handleClick(taskContent));
-
-  const deleteItem = document.createElement("i");
-  deleteItem.classList.add("far");
-  deleteItem.classList.add("fa-trash-alt");
-
-  deleteItem.addEventListener("click", () =>
-    handleDeleteClick(taskItemContainer, taskContent)
-  );
-
-  taskItemContainer.appendChild(taskContent);
-  taskItemContainer.appendChild(deleteItem);
-
-  tasksContainer.appendChild(taskItemContainer);
-
-  inputElement.value = "";
-};
-
-const handleClick = (taskContent) => {
-  const tasks = tasksContainer.childNodes;
-
-  for (const task of tasks) {
-    if (task.firstChild.isSameNode(taskContent)) {
-      task.firstChild.classList.toggle("completed");
+var inputElement = document.querySelector(".new-task-input");
+var addTaskButton = document.querySelector(".new-task-button");
+var tasksContainer = document.querySelector(".tasks-container");
+var validateInput = function () { return inputElement.value.trim().length > 0; };
+var handleAddTask = function () {
+    var inputIsValid = validateInput();
+    console.log(inputElement);
+    if (!inputIsValid) {
+        return inputElement.classList.add("error");
     }
-  }
+    var taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+    var taskContent = document.createElement("p");
+    taskContent.innerText = inputElement.value;
+    taskContent.addEventListener("click", function () { return handleClick(taskContent); });
+    var deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+    deleteItem.addEventListener("click", function () {
+        return handleDeleteClick(taskItemContainer, taskContent);
+    });
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+    tasksContainer.appendChild(taskItemContainer);
+    inputElement.value = "";
 };
-
-const handleDeleteClick = (taskItemContainer, taskContent) => {
-  const tasks = tasksContainer.childNodes;
-
-  for (const task of tasks) {
-    if (task.firstChild.isSameNode(taskContent)) {
-      taskItemContainer.remove();
+var handleClick = function (taskContent) {
+    var tasks = tasksContainer.childNodes;
+    for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+        var task = tasks_1[_i];
+        console.log(task.firstChild);
+        if (task.firstChild.isSameNode(taskContent)) {
+            task.firstChild.classList.toggle("completed");
+        }
     }
-  }
 };
-
-const handleInputChange = () => {
-  const inputIsValid = validateInput();
-
-  if (inputIsValid) {
-    return inputElement.classList.remove("error");
-  }
+var handleDeleteClick = function (taskItemContainer, taskContent) {
+    var tasks = tasksContainer.childNodes;
+    for (var _i = 0, tasks_2 = tasks; _i < tasks_2.length; _i++) {
+        var task = tasks_2[_i];
+        if (task.firstChild.isSameNode(taskContent)) {
+            taskItemContainer.remove();
+        }
+    }
 };
-
-addTaskButton.addEventListener("click", () => handleAddTask());
-
-inputElement.addEventListener("change", () => handleInputChange());
+var handleInputChange = function () {
+    var inputIsValid = validateInput();
+    if (inputIsValid) {
+        return inputElement.classList.remove("error");
+    }
+};
+addTaskButton.addEventListener("click", handleAddTask);
+inputElement.addEventListener("change", handleInputChange);
---
+++ index.ts
@@ -0,0 +1,79 @@
+const inputElement: HTMLInputElement =
+  document.querySelector(".new-task-input");
+const addTaskButton = document.querySelector(".new-task-button");
+
+const tasksContainer = document.querySelector(".tasks-container");
+
+const validateInput = () => inputElement.value.trim().length > 0;
+
+const handleAddTask = () => {
+  const inputIsValid = validateInput();
+
+  console.log(inputElement);
+
+  if (!inputIsValid) {
+    return inputElement.classList.add("error");
+  }
+
+  const taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("task-item");
+
+  const taskContent = document.createElement("p");
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick(taskContent));
+
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("far");
+  deleteItem.classList.add("fa-trash-alt");
+
+  deleteItem.addEventListener("click", () =>
+    handleDeleteClick(taskItemContainer, taskContent)
+  );
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemContainer);
+
+  inputElement.value = "";
+};
+
+const handleClick = (taskContent: HTMLParagraphElement) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    console.log(task.firstChild);
+
+    if (task.firstChild.isSameNode(taskContent)) {
+      (task.firstChild as HTMLElement).classList.toggle("completed");
+    }
+  }
+};
+
+const handleDeleteClick = (
+  taskItemContainer: HTMLDivElement,
+  taskContent: HTMLParagraphElement
+) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    if (task.firstChild.isSameNode(taskContent)) {
+      taskItemContainer.remove();
+    }
+  }
+};
+
+const handleInputChange = () => {
+  const inputIsValid = validateInput();
+
+  if (inputIsValid) {
+    return inputElement.classList.remove("error");
+  }
+};
+
+addTaskButton.addEventListener("click", handleAddTask);
+
+inputElement.addEventListener("change", handleInputChange);
+
+export {};
